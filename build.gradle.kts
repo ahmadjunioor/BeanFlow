16b004c93fa9cf6b85ed8335bac33e159ccfe25d
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2026-01-13 16:06:13 +0700

    Tambahkan plugin Google Services untuk integrasi Firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// build.gradle.kts (Project Level)
 plugins {
+    // Biarkan 3 baris alias ini (JANGAN DIHAPUS)
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    // Tambahkan baris Firebase ini:
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
